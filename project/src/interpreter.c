6dc2a67b2fb4660046e576539f47124e71410233
claragoussanou <c.h.goussanou@gmail.com>
2026-01-31 20:28:28 -0500

    my_mkdir
    finished mkdir

--- project/src/interpreter.c
+++ project/src/interpreter.c
@@ -5,6 +5,8 @@
 #include "shell.h"
 #include <unistd.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <ctype.h>
 
 int MAX_ARGS_SIZE = 3;
 
@@ -17,6 +19,11 @@ int badcommand() {
 int badcommandFileDoesNotExist() {
     printf("Bad command: File not found\n");
     return 3;
+}
+
+int badcommandmkdir() {
+    printf("Bad command: my_mkdir\n");
+    return 4;
 }
 
 int comp(const void *a,const void *b) {
@@ -210,11 +217,42 @@ int my_ls() {
 }
 
 int my_mkdir(char *var) {
-    
-    return 0;
+    int len = strlen(var);
+    for (int i = 1; i < len; i++){
+        if (!isalnum(var[i])) {
+            return badcommandmkdir();
+        }
+    }
+
+    if (var[0] == '$') {
+        char *result = mem_get_value(&var[1]);
+        if (strcmp(result,"Variable does not exist")) {
+            int i = 0;
+            while (result[i] != '\0') {
+                if (!isalnum(result[i])) {
+                    return badcommandmkdir();
+                }
+                i++;
+            }
+            mkdir(result,755);
+            return 0;
+        }
+        
+    } else if (isalnum(var[0])) {
+        mkdir(var,755);
+        return 0;
+    }
+    return badcommandmkdir();
 }
 
 int my_touch(char *var) {
+    int len = strlen(var);
+    for (int i = 1; i < len; i++){
+        if (!isalnum(var[i])) {
+            return badcommandmkdir();
+        }
+    }
+
     FILE *fp = fopen(var,"w");
     fclose(fp);
     return 0;
