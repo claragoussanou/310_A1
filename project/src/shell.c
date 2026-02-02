75e95f918e64da2cb7e25eef385a152020e3ef0f
Angela <MiaoAngelaa@gmail.com>
2026-02-02 14:40:08 -0500

    oneliners

--- project/src/shell.c
+++ project/src/shell.c
@@ -7,10 +7,6 @@
 #include "shellmemory.h"
 
 int parseInput(char ui[]);
-struct instruction {
-    int instr_w;
-    char *instr_words[100];
-};
 
 // Start of everything
 int main(int argc, char *argv[]) {
@@ -28,7 +24,7 @@ int main(int argc, char *argv[]) {
     //init shell memory
     mem_init();
     while(1) {	
-        if(isatty(fileno(stdin))) {
+        if(isatty(fileno(stdin))) { 
             printf("%c ", prompt); 
         }			
         
@@ -47,15 +43,14 @@ int main(int argc, char *argv[]) {
 int wordEnding(char c) {
     // You may want to add ';' to this at some point,
     // or you may want to find a different way to implement chains.
-    return c == '\0' || c == '\n' || c == ' ';
+    return c == '\0' || c == '\n' || c == ' '|| c == ';';
 }
 
 int parseInput(char inp[]) {
     char tmp[200], *words[100];                            
-    int ix = 0, w = 0, instrlen = 0;
+    int ix = 0, w = 0;
     int wordlen;
     int errorCode;
-    struct instruction instructions[10];
 
     for (ix = 0; inp[ix] == ' ' && ix < 1000; ix++); // skip white spaces
     while (inp[ix] != '\n' && inp[ix] != '\0' && ix < 1000) {
@@ -65,19 +60,20 @@ int parseInput(char inp[]) {
         }
         tmp[wordlen] = '\0';
         words[w] = strdup(tmp);
+        //puts("end word");
         w++;
         if (inp[ix] == ';') {
-            struct instruction new_instr = {w, words};
-            
-            instructions[instrlen] = new_instr;
-            instrlen++;
+            errorCode = interpreter(words, w);
+            if (errorCode == -1) exit(99); // handle the error here instead of main
+            ix++;
+            char nextInp[strlen(inp)];
+            strcpy(nextInp,inp+ix);
+            return parseInput(nextInp);
         }
 
         if (inp[ix] == '\0') break;
         ix++; 
     }
-
-
 
     errorCode = interpreter(words, w);
     return errorCode;
